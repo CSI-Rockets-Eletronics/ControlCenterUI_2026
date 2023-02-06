24065e50c25fd390378427708bfb535cf1fe8014
Alex Chen <axchen7@outlook.com>
2023-02-06 01:23:57 -0500

    Add tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      red: "#ff0000",
+      orange: "#ff5c00",
+      yellow: "#ffe600",
+      green: "#00ac1c",
+    },
     extend: {},
   },
   plugins: [],
