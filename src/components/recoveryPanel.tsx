ea7e0589b31fe721706b556a5bc1db9511907a50
Alex Chen <axchen7@outlook.com>
2023-03-10 20:39:49 -0500

    Add return to launch mode button

--- src/components/recoveryPanel.tsx
+++ src/components/recoveryPanel.tsx
@@ -1,9 +1,46 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 
+import { Button } from "./design/button";
+import { Panel } from "./design/panel";
+import {
+  useLaunchMachineActorRef,
+  useLaunchMachineSelector,
+} from "./launchMachineProvider";
 import { MapPanel } from "./mapPanel";
 import { RangePermit } from "./rangePermit";
 import { StatusPanel } from "./statusPanel";
 import { WeatherPanel } from "./weatherPanel";
+
+const GoToLaunchModePanel = memo(function GoToLaunchModePanel() {
+  const launchActorRef = useLaunchMachineActorRef();
+
+  const goToLaunchModeDisabled = useLaunchMachineSelector(
+    (state) =>
+      !state.can({
+        type: "UPDATE_ACTIVE_PANEL",
+        value: "launch",
+      })
+  );
+
+  const handleGoToLaunchMode = useCallback(() => {
+    launchActorRef.send({
+      type: "UPDATE_ACTIVE_PANEL",
+      value: "launch",
+    });
+  }, [launchActorRef]);
+
+  return (
+    <Panel>
+      <Button
+        color="green"
+        disabled={goToLaunchModeDisabled}
+        onClick={handleGoToLaunchMode}
+      >
+        RETURN TO LAUNCH MODE
+      </Button>
+    </Panel>
+  );
+});
 
 export const RecoveryPanel = memo(function RecoveryPanel() {
   return (
@@ -13,7 +50,10 @@ export const RecoveryPanel = memo(function RecoveryPanel() {
         <WeatherPanel />
       </div>
       <MapPanel />
-      <StatusPanel />
+      <div className="grid grid-rows-[1fr,auto] gap-4">
+        <StatusPanel />
+        <GoToLaunchModePanel />
+      </div>
     </div>
   );
 });
