073952bf773646c37ec79f0d862acb57a6a332dc
Alex Chen <axchen7@outlook.com>
2023-03-24 17:19:24 -0400

    Make statusDisplay clickable

--- src/components/design/statusDisplay.tsx
+++ src/components/design/statusDisplay.tsx
@@ -1,29 +1,48 @@
 import { memo } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface Props {
+type Props = {
   label: string;
   color: "green" | "yellow";
   value: string;
-}
+} & (
+  | {
+      disabled: boolean;
+      onClick: () => void;
+    }
+  | {
+      disabled?: undefined;
+      onClick?: undefined;
+    }
+);
 
 export const StatusDisplay = memo(function StatusDisplay({
   label,
-  value,
   color,
+  value,
+  disabled,
+  onClick,
 }: Props) {
+  const blockClick = disabled || !onClick;
+
   return (
-    <div className="flex items-center px-4 py-3 border rounded-lg gap-6 bg-gray-bg-2 border-gray-border">
+    <div className="flex items-center px-4 py-3 border rounded-lg gap-6 text-gray-text bg-gray-bg-2 border-gray-border">
       <p className="text-gray-text">{label}</p>
-      <div
+      <button
         className={twMerge(
           "grow rounded-lg px-2 py-1.5 border text-center",
-          color === "green" && "bg-green-fallback-7 border-green-fallback-9",
-          color === "yellow" && "bg-yellow-fallback-7 border-yellow-fallback-9"
+          disabled && "opacity-50",
+          blockClick && "pointer-events-none",
+          color === "green" &&
+            "bg-green-fallback-7 border-green-fallback-9 hover:bg-green-fallback-8 active:bg-green-fallback-9",
+          color === "yellow" &&
+            "bg-yellow-fallback-7 border-yellow-fallback-9 hover:bg-yellow-fallback-8 active:bg-yellow-fallback-9"
         )}
+        disabled={blockClick}
+        onClick={onClick}
       >
-        <p className="text-gray-text">{value}</p>
-      </div>
+        {value}
+      </button>
     </div>
   );
 });
