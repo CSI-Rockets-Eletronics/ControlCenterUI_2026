159d8afae851bf46f08491ffb0603f3e378bf096
Alex Chen <axchen7@outlook.com>
2023-05-06 00:26:52 -0400

    Improve modal on mobile

--- src/components/messagesModal.tsx
+++ src/components/messagesModal.tsx
@@ -32,7 +32,7 @@ export const MessagesModal = memo(function MessagesModal({
   return (
     <div
       className={twMerge(
-        "absolute inset-0 flex items-center justify-center p-8",
+        "absolute inset-0 flex items-center justify-center",
         !open && "hidden"
       )}
     >
@@ -40,12 +40,13 @@ export const MessagesModal = memo(function MessagesModal({
         className="absolute inset-0 opacity-95 bg-gray-bg-1"
         onClick={onClose}
       />
-      <div className="relative w-full h-full max-w-7xl gap-4 grid grid-rows-1 grid-cols-2">
-        <div className="grid grid-rows-[minmax(0,1fr),auto] gap-4">
+      <div className="relative w-full h-full p-8 max-w-[1400px] gap-4 grid grid-rows-[auto,auto] md:grid-rows-1 md:grid-cols-2 scrollable">
+        <div className="absolute inset-0" onClick={onClose} />
+        <div className="z-0 grid grid-rows-[auto,auto] md:grid-rows-[minmax(0,1fr),auto] gap-4">
           <SentMessagesPanel />
           <SendManualMessagePanel />
         </div>
-        <div className="grid grid-rows-[auto,minmax(0,1fr)] gap-4">
+        <div className="z-0 grid grid-rows-[auto,auto] md:grid-rows-[auto,minmax(0,1fr)] gap-4">
           <PresetMessagesPanel />
           <MonitorRecordsPanel visible={open} />
         </div>
